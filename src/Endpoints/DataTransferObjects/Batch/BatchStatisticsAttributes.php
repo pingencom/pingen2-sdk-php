64149492986035c8253a3df4449be36c8c1b3ac0
pingencom <info@pingen.com>
2023-05-19 12:43:04 +0200

    add batch statistics

--- src/Endpoints/DataTransferObjects/Batch/BatchStatisticsAttributes.php
+++ src/Endpoints/DataTransferObjects/Batch/BatchStatisticsAttributes.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Pingen\Endpoints\DataTransferObjects\Batch;
 
-use Carbon\CarbonImmutable;
 use Pingen\Support\DataTransferObject\DataTransferObject;
 
 class BatchStatisticsAttributes extends DataTransferObject
