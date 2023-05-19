99e7e719f5451330eb0ee784e0fcc670ce0c66c2
pingencom <info@pingen.com>
2023-05-19 13:57:44 +0200

    Add batch statistics (#14)
    * add batch statistics

---
+++ src/Endpoints/DataTransferObjects/Batch/BatchStatisticsAttributes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pingen\Endpoints\DataTransferObjects\Batch;
+
+use Pingen\Support\DataTransferObject\DataTransferObject;
+
+class BatchStatisticsAttributes extends DataTransferObject
+{
+    public int $letter_validating;
+
+    public array $letter_groups;
+
+    public array $letter_countries;
+}
