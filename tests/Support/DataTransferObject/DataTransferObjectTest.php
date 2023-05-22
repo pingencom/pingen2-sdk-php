bed0715097198594a9908f6555d4dfa37c761856
pingencom <info@pingen.com>
2023-05-22 13:53:10 +0200

    improve tests

--- tests/Support/DataTransferObject/DataTransferObjectTest.php
+++ tests/Support/DataTransferObject/DataTransferObjectTest.php
@@ -4,31 +4,23 @@ declare(strict_types=1);
 
 namespace Tests\Support\DataTransferObject;
 
-use Pingen\Endpoints\DataTransferObjects\Letter\LetterAttributes;
 use Pingen\Support\DataTransferObject\DataTransferObject;
 use Pingen\Support\DataTransferObject\DataTransferObjectError;
 use Tests\TestCase;
 
 class DataTransferObjectTest extends TestCase
 {
-    public function testUninitialized(): void
-    {
-        try {
-            new DummyDTO();
-        } catch (DataTransferObjectError $e) {
-            $this->assertEquals("The following invalid types were encountered:\nexpected `Tests\Support\DataTransferObject\DummyDTO::status` to be of type `string`, instead got value `null`, which is NULL.\nexpected `Tests\Support\DataTransferObject\DummyDTO::name` to be of type `string`, instead got value `null`, which is NULL.\n", $e->getMessage());
-        }
-    }
-
     public function testInvalidTypeMessage(): void
     {
         try {
             new DummyDTO([
-                'status' => 'sent',
-                'name' => 1
+                'status' => null,
+                'name' => 1,
+                'object' => 'test',
+                'array' => 1
             ]);
         } catch (DataTransferObjectError $e) {
-            $this->assertEquals("Invalid type: expected `Tests\Support\DataTransferObject\DummyDTO::name` to be of type `string`, instead got value `1`, which is integer..", $e->getMessage());
+            $this->assertEquals("The following invalid types were encountered:\nexpected `Tests\Support\DataTransferObject\DummyDTO::status` to be of type `string`, instead got value `null`, which is NULL.\nexpected `Tests\Support\DataTransferObject\DummyDTO::name` to be of type `string`, instead got value `1`, which is integer.\nexpected `Tests\Support\DataTransferObject\DummyDTO::object` to be of type `object`, instead got value `test`, which is string.\nexpected `Tests\Support\DataTransferObject\DummyDTO::array` to be of type `array`, instead got value `1`, which is integer.\n", $e->getMessage());
         }
     }
 
@@ -55,7 +47,9 @@ class DataTransferObjectTest extends TestCase
         $this->assertSame([
             'status' => 'sent',
             'name' => 'name',
-            'price' => null
+            'price' => null,
+            'object' => null,
+            'array' => null
         ], $dto->all());
     }
 }
@@ -65,6 +59,8 @@ class DummyDTO extends DataTransferObject
     public string $status;
     public string $name;
     public ?int $price;
+    public ?object $object;
+    public ?array $array;
 
     protected bool $ignoreMissing = false;
 }
