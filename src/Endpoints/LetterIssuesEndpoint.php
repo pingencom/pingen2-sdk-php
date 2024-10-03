0d938c979ba37a8371b6bee739e4b64fd18b59ba
pingencom <info@pingen.com>
2024-10-03 11:33:02 +0200

    Resolve: "Add a new event endpoints" (#26)

--- src/Endpoints/LetterIssuesEndpoint.php
+++ src/Endpoints/LetterIssuesEndpoint.php
@@ -29,7 +29,7 @@ class LetterIssuesEndpoint extends ResourceEndpoint
         return new LetterEventCollection(
             $this
                 ->performGetCollectionRequest(
-                    sprintf('/organisations/%s/letters/issues', $this->getOrganisationId()),
+                    sprintf('/organisations/%s/letters/events/issues', $this->getOrganisationId()),
                     $letterIssuesCollectionParameterBag ?? (new LetterIssuesCollectionParameterBag())
                 )->json()
         );
--- tests/Endpoints/LetterEventsEndpointTest.php
+++ tests/Endpoints/LetterEventsEndpointTest.php
@@ -47,4 +47,103 @@ class LetterEventsEndpointTest extends EndpointTestBase
 
         $this->assertCount(1, $endpoint->getHttpClient()->recorded());
     }
+
+    public function testGetIssuesCollection(): void
+    {
+        $listParameterBag = (new LetterEventCollectionParameterBag())
+            ->setPageLimit(10)
+            ->setPageNumber(2)
+            ->setFieldsLetter(['status'])
+            ->setFieldsLetterEvent(['code'])
+            ->setLanguage('en-GB');
+
+        $endpoint = (new LetterEventsEndpoint($this->getAccessToken()))
+            ->setOrganisationId('example')
+            ->setLetterId('exampleLetterID');
+
+        $endpoint->getHttpClient()->fakeSequence()
+            ->push(
+                '{"data": [], "links": {"first": "string", "last": "string", "prev": null, "next": null, "self": "string"}, "meta": {"current_page": 2, "last_page": 3, "per_page": 10, "from": 10, "to": 19, "total": 30}}',
+                Response::HTTP_OK,
+            );
+
+        $endpoint->getIssuesCollection($listParameterBag);
+
+        $endpoint->getHttpClient()->recorded(
+            function (Request $request) use ($endpoint): void {
+                $this->assertEquals(
+                    $endpoint->getResourceBaseUrl() . '/organisations/example/letters/events/issues?page%5Blimit%5D=10&page%5Bnumber%5D=2&fields%5Bletters%5D=status&fields%5Bletters_events%5D=code&language=en-GB',
+                    $request->url()
+                );
+            }
+        );
+
+        $this->assertCount(1, $endpoint->getHttpClient()->recorded());
+    }
+
+    public function testGetSentCollection(): void
+    {
+        $listParameterBag = (new LetterEventCollectionParameterBag())
+            ->setPageLimit(10)
+            ->setPageNumber(2)
+            ->setFieldsLetter(['status'])
+            ->setFieldsLetterEvent(['code'])
+            ->setLanguage('en-GB');
+
+        $endpoint = (new LetterEventsEndpoint($this->getAccessToken()))
+            ->setOrganisationId('example')
+            ->setLetterId('exampleLetterID');
+
+        $endpoint->getHttpClient()->fakeSequence()
+            ->push(
+                '{"data": [], "links": {"first": "string", "last": "string", "prev": null, "next": null, "self": "string"}, "meta": {"current_page": 2, "last_page": 3, "per_page": 10, "from": 10, "to": 19, "total": 30}}',
+                Response::HTTP_OK,
+            );
+
+        $endpoint->getSentCollection($listParameterBag);
+
+        $endpoint->getHttpClient()->recorded(
+            function (Request $request) use ($endpoint): void {
+                $this->assertEquals(
+                    $endpoint->getResourceBaseUrl() . '/organisations/example/letters/events/sent?page%5Blimit%5D=10&page%5Bnumber%5D=2&fields%5Bletters%5D=status&fields%5Bletters_events%5D=code&language=en-GB',
+                    $request->url()
+                );
+            }
+        );
+
+        $this->assertCount(1, $endpoint->getHttpClient()->recorded());
+    }
+
+    public function testGetUndeliverableCollection(): void
+    {
+        $listParameterBag = (new LetterEventCollectionParameterBag())
+            ->setPageLimit(10)
+            ->setPageNumber(2)
+            ->setFieldsLetter(['status'])
+            ->setFieldsLetterEvent(['code'])
+            ->setLanguage('en-GB');
+
+        $endpoint = (new LetterEventsEndpoint($this->getAccessToken()))
+            ->setOrganisationId('example')
+            ->setLetterId('exampleLetterID');
+
+        $endpoint->getHttpClient()->fakeSequence()
+            ->push(
+                '{"data": [], "links": {"first": "string", "last": "string", "prev": null, "next": null, "self": "string"}, "meta": {"current_page": 2, "last_page": 3, "per_page": 10, "from": 10, "to": 19, "total": 30}}',
+                Response::HTTP_OK,
+            );
+
+        $endpoint->getUndeliverableCollection($listParameterBag);
+
+        $endpoint->getHttpClient()->recorded(
+            function (Request $request) use ($endpoint): void {
+                $this->assertEquals(
+                    $endpoint->getResourceBaseUrl() . '/organisations/example/letters/events/undeliverable?page%5Blimit%5D=10&page%5Bnumber%5D=2&fields%5Bletters%5D=status&fields%5Bletters_events%5D=code&language=en-GB',
+                    $request->url()
+                );
+            }
+        );
+
+        $this->assertCount(1, $endpoint->getHttpClient()->recorded());
+    }
 }
